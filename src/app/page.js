d9e8e8de0b5083d6d55d8894096719a3b39b73c5
cyxkat157 <svhbatb0n@gmail.com>
2024-05-07 23:49:51 +0800

    doing

--- src/app/page.js
+++ src/app/page.js
@@ -17,10 +17,31 @@ export default function page() {
           <Ring />
         </div>
       </header>
-
       <div className={styles.profileContainer}>
-        <img src="" alt="" className={styles.profileImg} />
-        <div className={styles.profileInfo}></div>
+        <img
+          className={styles.profileImg}
+          src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
+          alt=""
+        />
+        <img
+          className={styles.profileImgLogo}
+          src="./554d8eb2db2055b525bfa9abdf1d56ee.webp"
+          alt=""
+        />
+        <div className={styles.profileInfo}>
+          <p>Оройн мэнд!</p>
+          <h1>Э.сүхбат</h1>
+          <h3>110781002349@e-mongolia.mn</h3>
+        </div>
+      </div>
+      <div className={styles.id}>
+        <div className={styles.idheader}>
+          <p>Миний бичиг баримтууд</p>
+          <div>
+            <p>шинэчлэх</p>
+            <div className="refresh"></div>
+          </div>
+        </div>
       </div>
     </body>
   );
